9a8af8b3632c08783f475a64393f9d27df6d1bdf
Aman Chandra <aman123chandra@gmail.com>
2024-08-14 01:06:21 +0530

    Adding Assignment

---
+++ DSA_Day_3/Assignment/TwoPattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main()
+ {
+    for ( int i = 1; i <=5; i++)
+    {
+        for (int j =1 ; j <= 5; j++)
+        {
+             if (i==1 || i==5 || i+j ==6 )
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        
+            
+        }
+        printf("\n");
+        
+
+        
+    }
+    
+}
+// Output:
+
+// *
+// *
+// *
+// *
+// *
